51b37968cb4bfc7fcf0cf5c03bb78d5afcb1aa73
HarvM <marc.harvey2@gmail.com>
2021-06-09 18:16:38 +0100

    ScrollView + Rough Layout of LazyVGrid components
    Have adjusted the frame and the layout of each of the LazyVGrid sections. Added in a  ScrollView too to ensure it can all be seen

---
+++ ShoppingItems/ViewExtensions/ThemesViewExtension.swift
@@ -0,0 +1,22 @@
+//
+//  ThemesViewExtension.swift
+//  Yer Messages
+//
+//  Created by Marc Harvey on 09/06/2021.
+//  Copyright © 2021 Marc Harvey. All rights reserved.
+//
+
+import Foundation
+
+extension ThemesView {
+    public func buyNewTheme() {
+        print("Bought the new theme")
+        
+        //TODO: Have the button buy and then change the theme
+        
+        //Firstly purchase the theme
+        
+        //Once installed: the themes can then be switched so allow it to toggled
+        
+    }
+}
--- ShoppingItems/Views/HamburgerMenuViews/ThemesView.swift
+++ ShoppingItems/Views/HamburgerMenuViews/ThemesView.swift
@@ -9,14 +9,41 @@
 import SwiftUI
 
 struct ThemesView: View {
+    
     @Environment(\.presentationMode) var presentationMode
+    private var themeNames = ["Bobcat", "Stratos", "Enigma", "Belter"]
+    private var themeDetails = ["Fun", "Cool", "Something else", "Hello there"]
+    private var oneColumnGrid = [GridItem(.flexible())]
+    private var imagesForGrid = [ContentViewImages.appIcon.rawValue, ContentViewImages.appIcon.rawValue, ContentViewImages.appIcon.rawValue, ContentViewImages.appIcon.rawValue]
     
     var body: some View {
         NavigationView {
             ZStack {
                 Color(BackgroundColours.defaultBackground.rawValue).edgesIgnoringSafeArea(.all)
-                VStack {
-                    //TODO: Enter in content here
+                GeometryReader { geometry in
+                    ScrollView {
+                        VStack {
+                            LazyVGrid(columns: oneColumnGrid) {
+                                ForEach((0...3), id: \.self) {
+                                    ///Theme Name
+                                    Text("Theme name: \(themeNames[$0 % themeNames.count])")
+                                        .font(.callout)
+                                    ///Extra Info
+                                    Text("Extra information: \(themeDetails[$0 % themeDetails.count])")
+                                        .font(.footnote)
+                                    ///Image of the theme (placeholder for now)
+                                    Image("\(imagesForGrid[$0 % imagesForGrid.count])")
+                                        .frame(width: 50, height: 50, alignment: .center)
+                                    Button(action: self.buyNewTheme, label: {
+                                        Text("Purchase New Theme")
+                                            .font(.footnote)
+                                    })
+                                }
+                                ///Sorts the frame of all of the LazyVGrid
+                                .frame(width: geometry.size.width, height: geometry.size.height/9, alignment: .center)
+                            }
+                        }
+                    }
                 }
             }
             .toolbar {
